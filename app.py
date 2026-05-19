2796a91ff43d71037ce12285c56ff97be14e6828
StalinBarraza <stalin.barraza@upb.edu.co>
2026-05-18 21:03:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -756,7 +756,7 @@ if modo == 'Predicción Manual':
                 st.markdown(f'''
                     <div class="hero-num live">{res_prediccion:.1f}</div>
                     <div class="hero-unit"> Cargas / Turno</div>
-                    <div class="hero-range">Rango esperado: {lower_bound:.1f} - {upper_bound:.1f} K Ton</div>
+                    <div class="hero-range">Rango esperado: {lower_bound:.1f} - {upper_bound:.1f} Cargas </div>
                 ''', unsafe_allow_html=True)
 
                 st.markdown(f'''
