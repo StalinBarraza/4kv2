49e18edb806bc6df887d599ccf45f7b19000c8ed
StalinBarraza <stalin.barraza@upb.edu.co>
2026-05-22 16:51:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
     --red-hi:     #E8253F;
     --red-dim:    rgba(200,16,46,0.14);
     --red-glow:   rgba(200,16,46,0.25);
-    --bg-app:     #FAFAFA;
+    --bg-app:     #0A0909;
     --bg-card:    #161719;
     --bg-elev:    #1E2024;
     --bg-deep:    #09090B;
